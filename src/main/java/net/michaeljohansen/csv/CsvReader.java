21186ff07885ddb130830352b0b98842d4d94644
Michael Johansen <michaeljohansen83@gmail.com>
2016-03-12 00:12:26 +0100

    feature: lines starting with # is ignored
    basic commenting

--- src/main/java/net/michaeljohansen/csv/CsvReader.java
+++ src/main/java/net/michaeljohansen/csv/CsvReader.java
@@ -3,213 +3,279 @@ package net.michaeljohansen.csv;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
 public class CsvReader {
-    private final Reader reader;
-    private List<List<String>> lazyResult = null;
-    private StringBuilder recordBuilder;
-    private List<String> records;
-    private State state;
-    private List<List<String>> rows;
-    private final Map<Operation, Map<State, Consumer<Character>>> operationStateMap = buildOperationMap();
-    private final Map<Character, Operation> characterOperationMap;
-    private static final Consumer<Character> NOOP = character -> {
+  private static final Consumer<Character> NO_OP = character -> {
+  };
+  private final Reader reader;
+  private final Map<Character, Operation> characterOperationMap;
+  private List<List<String>> lazyResult = null;
+  private StringBuilder recordBuilder;
+  private List<String> records;
+  private State state;
+  private List<List<String>> rows;
+  private final Map<Operation, Map<State, Consumer<Character>>> operationStateMap = buildOperationMap();
+
+  public CsvReader(String inputString) {
+    this(inputString, ',');
+  }
+
+  public CsvReader(String inputString, char delimiter) {
+    this(
+        new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8)),
+        delimiter,
+        StandardCharsets.UTF_8
+    );
+  }
+
+  public CsvReader(InputStream inputStream) {
+    this(inputStream, ',', StandardCharsets.UTF_8);
+  }
+
+  public CsvReader(InputStream inputStream, char delimiter, Charset charset) {
+    reader = new InputStreamReader(inputStream, charset);
+    characterOperationMap = buildCharacterOperationMap(delimiter);
+  }
+
+  public synchronized List<List<String>> getRows() {
+    if (lazyResult == null) {
+      state = State.OUTSIDE_VALUE;
+      recordBuilder = new StringBuilder();
+      clearRecords();
+      rows = new ArrayList<>();
+      int rawChar;
+      while ((rawChar = softRead()) != -1) {
+        char character = (char) rawChar;
+        Operation operation = getOperation(character);
+        Consumer<Character> characterConsumer = getCharacterConsumer(operation);
+        characterConsumer.accept(character);
+      }
+      close();
+      this.lazyResult = rows;
+    }
+    return lazyResult;
+  }
+
+  private Consumer<Character> getCharacterConsumer(Operation operation) {
+    Map<State, Consumer<Character>> stateMap = operationStateMap.get(
+        operation
+    );
+    if (stateMap == null){
+      throw new IllegalStateException(String.format(
+          "no stateMap for operation: %s",
+          operation
+      ));
+    }
+    Consumer<Character> characterConsumer = stateMap.get(state);
+    if (characterConsumer == null){
+      throw new IllegalStateException(String.format(
+          "no characterConsumer for operation: %s and state: %s",
+          operation,
+          state
+      ));
+    }
+    return characterConsumer;
+  }
+
+  public List<Map<String, String>> getRowsBasedOnHeaders() {
+    List<List<String>> rows = getRows();
+    if (rows.isEmpty()) {
+      throw new IllegalStateException("No lazyResult");
+    }
+    List<String> strings = rows.remove(0);
+    return rows.stream()
+        .map(row -> IntStream.range(0, strings.size())
+            .boxed()
+            .collect(toMap(strings::get, row::get))
+        )
+        .collect(toList());
+  }
+
+  private Operation getOperation(char character) {
+    if (characterOperationMap.containsKey(character)) {
+      return characterOperationMap.get(character);
+    }
+    return Operation.CHARACTER;
+  }
+
+  private int softRead() {
+    try {
+      return reader.read();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void close() {
+    try {
+      reader.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Map<Operation, Map<State, Consumer<Character>>> buildOperationMap() {
+    Map<Operation, Map<State, Consumer<Character>>> map = new HashMap<>();
+    map.put(Operation.QUOTE, getQuoteOperations());
+    map.put(Operation.CHARACTER, getCharacterOperations());
+    map.put(Operation.DELIMITER, getDelimiterOperations());
+    map.put(Operation.NEWLINE, getNewlineOperations());
+    map.put(Operation.IGNORE, getIgnoreOperations());
+    map.put(Operation.START_COMMENT, getCommentOperations());
+    return map;
+  }
+
+  private Map<State, Consumer<Character>> getIgnoreOperations() {
+    Map<State, Consumer<Character>> operations = new HashMap<>();
+    operations.put(State.INSIDE_QUOTED_VALUE, NO_OP);
+    operations.put(State.INSIDE_VALUE, NO_OP);
+    operations.put(State.QUOTE, NO_OP);
+    operations.put(State.OUTSIDE_VALUE, NO_OP);
+    operations.put(State.COMMENT, NO_OP);
+    return operations;
+  }
+
+  private Map<State, Consumer<Character>> getNewlineOperations() {
+    Map<State, Consumer<Character>> operations = new HashMap<>();
+    Consumer<Character> finishRecordAndNewline = character -> {
+      state = State.OUTSIDE_VALUE;
+      addBufferToRecords();
+      appendRecordToRows();
+      clearRecords();
     };
+    operations.put(State.OUTSIDE_VALUE, finishRecordAndNewline);
+    operations.put(State.QUOTE, finishRecordAndNewline);
+    operations.put(State.INSIDE_VALUE, finishRecordAndNewline);
+    operations.put(
+        State.INSIDE_QUOTED_VALUE,
+        (character) -> getRecordBuilder().append(character)
+    );
+    operations.put(State.COMMENT, character -> state = State.OUTSIDE_VALUE);
+    return operations;
+  }
 
-    public CsvReader(String inputString) {
-        this(inputString, ',');
-    }
+  private Map<State, Consumer<Character>> getDelimiterOperations() {
+    Map<State, Consumer<Character>> operations = new HashMap<>();
+    Consumer<Character> finishRecord = character -> {
+      state = State.OUTSIDE_VALUE;
+      addBufferToRecords();
+    };
+    operations.put(State.OUTSIDE_VALUE, finishRecord);
+    operations.put(State.INSIDE_VALUE, finishRecord);
+    operations.put(State.QUOTE, finishRecord);
+    operations.put(
+        State.INSIDE_QUOTED_VALUE,
+        (character) -> getRecordBuilder().append(character)
+    );
+    operations.put(State.COMMENT, NO_OP);
+    return operations;
+  }
 
-    public CsvReader(String inputString, char delimiter) {
-        this(new ByteArrayInputStream(inputString.getBytes(StandardCharsets.UTF_8)), delimiter, StandardCharsets.UTF_8);
-    }
+  private Map<State, Consumer<Character>> getCharacterOperations() {
+    Map<State, Consumer<Character>> operations = new HashMap<>();
+    operations.put(State.OUTSIDE_VALUE, character -> {
+      state = State.INSIDE_VALUE;
+      getRecordBuilder().append(character);
+    });
+    operations.put(
+        State.INSIDE_VALUE,
+        (character) -> getRecordBuilder().append(character)
+    );
+    operations.put(
+        State.INSIDE_QUOTED_VALUE,
+        (character) -> getRecordBuilder().append(character)
+    );
+    operations.put(State.QUOTE, character -> {
+      throw new IllegalStateException("Met character after quote");
+    });
+    operations.put(State.COMMENT, NO_OP);
+    return operations;
+  }
 
-    public CsvReader(InputStream inputStream) {
-        this(inputStream, ',', StandardCharsets.UTF_8);
-    }
+  private Map<State, Consumer<Character>> getQuoteOperations() {
+    Map<State, Consumer<Character>> operations = new HashMap<>();
+    operations.put(
+        State.OUTSIDE_VALUE,
+        character -> state = State.INSIDE_QUOTED_VALUE
+    );
+    operations.put(
+        State.INSIDE_QUOTED_VALUE,
+        character -> state = State.QUOTE
+    );
+    operations.put(State.QUOTE, character -> {
+      state = State.INSIDE_QUOTED_VALUE;
+      getRecordBuilder().append(character);
+    });
+    operations.put(State.INSIDE_VALUE, character -> {
+      throw new IllegalStateException(String.format(
+          "Met quote inside unescaped value, message so far [%s]",
+          recordBuilder
+      ));
+    });
+    operations.put(State.COMMENT, NO_OP);
+    return operations;
+  }
 
-    public CsvReader(InputStream inputStream, char delimiter, Charset charset) {
-        reader = new InputStreamReader(inputStream, charset);
-        characterOperationMap = buildCharacterOperationMap(delimiter);
-    }
+  private Map<State, Consumer<Character>> getCommentOperations() {
+    // Consumed as normal, if not outside value
+    Map<State, Consumer<Character>> operations = getCharacterOperations();
+    operations.put(
+        State.OUTSIDE_VALUE,
+        character -> state = State.COMMENT
+    );
+    return operations;
+  }
 
-    public synchronized List<List<String>> getRows() {
-        if (lazyResult == null) {
-            state = State.OUTSIDE_VALUE;
-            recordBuilder = new StringBuilder();
-            clearRecords();
-            rows = new ArrayList<>();
-            int rawChar;
-            while ((rawChar = softRead()) != -1) {
-                char character = (char) rawChar;
-                Operation operation = getOperation(character);
-                Map<State, Consumer<Character>> stateMap = operationStateMap.get(operation);
-                Consumer<Character> characterConsumer = stateMap.get(state);
-                characterConsumer.accept(character);
-            }
-            close();
-            this.lazyResult = rows;
-        }
-        return lazyResult;
-    }
+  private synchronized void clearRecords() {
+    records = new ArrayList<>();
+  }
 
-    public List<Map<String, String>> getRowsBasedOnHeaders() {
-        List<List<String>> rows = getRows();
-        if (rows.isEmpty()) throw new IllegalStateException("No lazyResult");
-        List<String> strings = rows.remove(0);
-        return rows.stream().map(
-                row -> {
-                    Map<String, String> map = new HashMap<>();
-                    IntStream.range(0, strings.size())
-                            .forEach(index -> map.put(strings.get(index), row.get(index)));
-                    return map;
-                }
-        ).collect(Collectors.toList());
-    }
+  private synchronized void appendRecordToRows() {
+    rows.add(records);
+  }
 
-    private Operation getOperation(char character) {
-        if (characterOperationMap.containsKey(character)) {
-            return characterOperationMap.get(character);
-        }
-        return Operation.CHARACTER;
-    }
+  private synchronized void addBufferToRecords() {
+    records.add(getRecordBuilder().toString());
+    recordBuilder = new StringBuilder();
+  }
 
-    private int softRead() {
-        try {
-            return reader.read();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  private StringBuilder getRecordBuilder() {
+    return recordBuilder;
+  }
 
-    private void close() {
-        try {
-            reader.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  private Map<Character, Operation> buildCharacterOperationMap(char delimiter) {
+    Map<Character, Operation> map = new HashMap<>();
+    map.put(delimiter, Operation.DELIMITER);
+    map.put('\n', Operation.NEWLINE);
+    map.put('\r', Operation.IGNORE);
+    map.put('"', Operation.QUOTE);
+    map.put('#', Operation.START_COMMENT);
+    return map;
+  }
 
-    private Map<Operation, Map<State, Consumer<Character>>> buildOperationMap() {
-        Map<Operation, Map<State, Consumer<Character>>> map = new HashMap<>();
-        map.put(Operation.QUOTE, getQuouteOperations());
-        map.put(Operation.CHARACTER, getCharacterOperations());
-        map.put(Operation.DELIMITER, getDelimeterOperations());
-        map.put(Operation.NEWLINE, getNewlineOperations());
-        map.put(Operation.IGNORE, getIgnoreOperations());
-        return map;
-    }
+  enum State {
+    OUTSIDE_VALUE,
+    INSIDE_VALUE,
+    INSIDE_QUOTED_VALUE,
+    COMMENT,
+    QUOTE
+  }
 
-    private Map<State, Consumer<Character>> getIgnoreOperations() {
-        Map<State, Consumer<Character>> ignoreOperations = new HashMap<>();
-        ignoreOperations.put(State.INSIDE_QUOTED_VALUE, NOOP);
-        ignoreOperations.put(State.INSIDE_VALUE, NOOP);
-        ignoreOperations.put(State.QUOTE, NOOP);
-        ignoreOperations.put(State.OUTSIDE_VALUE, NOOP);
-        return ignoreOperations;
-    }
-
-    private Map<State, Consumer<Character>> getNewlineOperations() {
-        Map<State, Consumer<Character>> newlineOperations = new HashMap<>();
-        Consumer<Character> finishRecordAndNewline = character -> {
-            state = State.OUTSIDE_VALUE;
-            addBufferToRecords();
-            appendRecordToRows();
-            clearRecords();
-        };
-        newlineOperations.put(State.OUTSIDE_VALUE, finishRecordAndNewline);
-        newlineOperations.put(State.QUOTE, finishRecordAndNewline);
-        newlineOperations.put(State.INSIDE_VALUE, finishRecordAndNewline);
-        newlineOperations.put(State.INSIDE_QUOTED_VALUE, (character) -> getRecordBuilder().append(character));
-        return newlineOperations;
-    }
-
-    private Map<State, Consumer<Character>> getDelimeterOperations() {
-        Map<State, Consumer<Character>> delimiterOperations = new HashMap<>();
-        Consumer<Character> finishRecord = character -> {
-            state = State.OUTSIDE_VALUE;
-            addBufferToRecords();
-        };
-        delimiterOperations.put(State.OUTSIDE_VALUE, finishRecord);
-        delimiterOperations.put(State.INSIDE_VALUE, finishRecord);
-        delimiterOperations.put(State.QUOTE, finishRecord);
-        delimiterOperations.put(State.INSIDE_QUOTED_VALUE, (character) -> getRecordBuilder().append(character));
-        return delimiterOperations;
-    }
-
-    private Map<State, Consumer<Character>> getCharacterOperations() {
-        Map<State, Consumer<Character>> characterOperations = new HashMap<>();
-        characterOperations.put(State.OUTSIDE_VALUE, character -> {
-            state = State.INSIDE_VALUE;
-            getRecordBuilder().append(character);
-        });
-        characterOperations.put(State.INSIDE_VALUE, (character) -> getRecordBuilder().append(character));
-        characterOperations.put(State.INSIDE_QUOTED_VALUE, (character) -> getRecordBuilder().append(character));
-        characterOperations.put(State.QUOTE, character -> {
-            throw new IllegalStateException("Met character after quote");
-        });
-        return characterOperations;
-    }
-
-    private Map<State, Consumer<Character>> getQuouteOperations() {
-        Map<State, Consumer<Character>> quoteOperations = new HashMap<>();
-        quoteOperations.put(State.OUTSIDE_VALUE, character -> state = State.INSIDE_QUOTED_VALUE);
-        quoteOperations.put(State.INSIDE_QUOTED_VALUE, character -> state = State.QUOTE);
-        quoteOperations.put(State.QUOTE, character -> {
-            state = State.INSIDE_QUOTED_VALUE;
-            getRecordBuilder().append(character);
-        });
-        quoteOperations.put(State.INSIDE_VALUE, character -> {
-            throw new IllegalStateException(String.format(
-                    "Met quote inside unescaped value, message so far [%s]",
-                    recordBuilder
-            ));
-        });
-        return quoteOperations;
-    }
-
-    private synchronized void clearRecords() {
-        records = new ArrayList<>();
-    }
-
-    private synchronized void appendRecordToRows() {
-        rows.add(records);
-    }
-
-    private synchronized void addBufferToRecords() {
-        records.add(getRecordBuilder().toString());
-        recordBuilder = new StringBuilder();
-    }
-
-    private StringBuilder getRecordBuilder() {
-        return recordBuilder;
-    }
-
-    private Map<Character, Operation> buildCharacterOperationMap(char delimiter) {
-        Map<Character, Operation> map = new HashMap<>();
-        map.put(delimiter, Operation.DELIMITER);
-        map.put('\n', Operation.NEWLINE);
-        map.put('\r', Operation.IGNORE);
-        map.put('"', Operation.QUOTE);
-        return map;
-    }
-
-    enum State {
-        OUTSIDE_VALUE,
-        INSIDE_VALUE,
-        INSIDE_QUOTED_VALUE,
-        QUOTE
-    }
-
-    enum Operation {
-        QUOTE,
-        DELIMITER,
-        NEWLINE,
-        IGNORE,
-        CHARACTER
-    }
+  enum Operation {
+    QUOTE,
+    DELIMITER,
+    NEWLINE,
+    IGNORE,
+    START_COMMENT,
+    CHARACTER
+  }
 }
--- src/test/java/net/michaeljohansen/csv/CsvReaderTest.java
+++ src/test/java/net/michaeljohansen/csv/CsvReaderTest.java
@@ -6,141 +6,196 @@ import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class CsvReaderTest {
-    @Test
-    public void adjacentFieldsAreSeparatedWithComma() throws Exception {
-        CsvReader csvReader = new CsvReader("1997,Ford,E350\n");
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void adjacentFieldsAreSeparatedWithComma() {
+    CsvReader csvReader = new CsvReader("1997,Ford,E350\n");
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(1));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(1));
 
-        List<String> rowValues = rows.get(0);
-        assertThat(rowValues, hasItems("1997", "Ford", "E350"));
-    }
+    List<String> rowValues = rows.get(0);
+    assertThat(rowValues, hasItems("1997", "Ford", "E350"));
+  }
 
-    @Test
-    public void canHandleWindowNewline() throws Exception {
-        CsvReader csvReader = new CsvReader("1\r\n,2\r\n");
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void canHandleWindowNewline() {
+    CsvReader csvReader = new CsvReader("1\r\n,2\r\n");
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(2));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(2));
 
-        assertThat(rows.get(0), hasItems("1"));
-        assertThat(rows.get(1), hasItems("2"));
-    }
+    assertThat(rows.get(0), hasItems("1"));
+    assertThat(rows.get(1), hasItems("2"));
+  }
 
-    @Test
-    public void adjacentFieldsAreSeparatedWithCommaWithStream() throws Exception {
-        CsvReader csvReader = new CsvReader(new ByteArrayInputStream("1997,Ford,E350\n".getBytes(StandardCharsets.UTF_8)));
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void adjacentFieldsAreSeparatedWithCommaWithStream() {
+    CsvReader csvReader = new CsvReader(new ByteArrayInputStream(
+        "1997,Ford,E350\n".getBytes(StandardCharsets.UTF_8)));
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(1));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(1));
 
-        List<String> rowValues = rows.get(0);
-        assertThat(rowValues, hasItems("1997", "Ford", "E350"));
-    }
+    List<String> rowValues = rows.get(0);
+    assertThat(rowValues, hasItems("1997", "Ford", "E350"));
+  }
 
-    @Test
-    public void adjacentFieldsAreSeparatedWithSemicolon() throws Exception {
-        CsvReader csvReader = new CsvReader("1997;Ford;E350\n", ';');
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void adjacentFieldsAreSeparatedWithSemicolon() {
+    CsvReader csvReader = new CsvReader("1997;Ford;E350\n", ';');
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(1));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(1));
 
-        List<String> rowValues = rows.get(0);
-        assertThat(rowValues, hasItems("1997", "Ford", "E350"));
-    }
+    List<String> rowValues = rows.get(0);
+    assertThat(rowValues, hasItems("1997", "Ford", "E350"));
+  }
 
-    @Test
-    public void anyFieldMayBeQuoted() throws Exception {
-        CsvReader csvReader = new CsvReader("\"1997\",\"Ford\",\"E350\"\n");
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void anyFieldMayBeQuoted() {
+    CsvReader csvReader = new CsvReader("\"1997\",\"Ford\",\"E350\"\n");
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(1));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(1));
 
-        List<String> rowValues = rows.get(0);
-        assertThat(rowValues, hasItems("1997", "Ford", "E350"));
-    }
+    List<String> rowValues = rows.get(0);
+    assertThat(rowValues, hasItems("1997", "Ford", "E350"));
+  }
 
-    @Test
-    public void fieldsWithEmbeddedCommasOrDoubleQuoteCharactersMustBeQuoted() throws Exception {
-        CsvReader csvReader = new CsvReader("1997,Ford,E350,\"Super, luxurious truck\"\n");
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void fieldsWithEmbeddedCommasOrDoubleQuoteCharactersMustBeQuoted() {
+    CsvReader csvReader = new CsvReader(
+        "1997,Ford,E350,\"Super, luxurious truck\"\n"
+    );
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(1));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(1));
 
-        List<String> rowValues = rows.get(0);
-        assertThat(rowValues, hasItems("1997", "Ford", "E350", "Super, luxurious truck"));
-    }
+    List<String> rowValues = rows.get(0);
+    assertThat(
+        rowValues,
+        hasItems("1997", "Ford", "E350", "Super, luxurious truck")
+    );
+  }
 
-    @Test
-    public void eachOfTheEmbeddedDoubleQuoteCharactersMustBeRepresentedByAPairOfDoubleQuoteCharacters() throws Exception {
-        CsvReader csvReader = new CsvReader("1997,Ford,E350,\"Super, \"\"luxurious\"\" truck\"\n");
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void shouldAcceptQuoteInValues() {
+    CsvReader csvReader = new CsvReader(
+        "1997,Ford,E350,\"Super, \"\"luxurious\"\" truck\"\n"
+    );
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(1));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(1));
 
-        List<String> rowValues = rows.get(0);
-        assertThat(rowValues, hasItems("1997", "Ford", "E350", "Super, \"luxurious\" truck"));
-    }
+    List<String> rowValues = rows.get(0);
+    assertThat(
+        rowValues,
+        hasItems(
+            "1997",
+            "Ford",
+            "E350",
+            "Super, \"luxurious\" truck"
+        )
+    );
+  }
 
-    @Test
-    public void fieldsWithEmbeddedLineBreaksMustBeQuoted() throws Exception {
-        CsvReader csvReader = new CsvReader("1997,Ford,E350,\"Go get one now\nthey are going fast\"\n");
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void fieldsWithEmbeddedLineBreaksMustBeQuoted() {
+    CsvReader csvReader = new CsvReader(
+        "1997,Ford,E350,\"Go get one now\nthey are going fast\"\n"
+    );
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(1));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(1));
 
-        List<String> rowValues = rows.get(0);
-        assertThat(rowValues, hasItems("1997", "Ford", "E350", "Go get one now\nthey are going fast"));
-    }
+    List<String> rowValues = rows.get(0);
+    assertThat(
+        rowValues,
+        hasItems("1997", "Ford", "E350", "Go get one now\nthey are going fast")
+    );
+  }
 
-    @Test
-    public void spacesAreConsideredPartOfAFieldAndShouldNotBeIgnored() throws Exception {
-        CsvReader csvReader = new CsvReader("1997, Ford, E350\n");
-        List<List<String>> rows = csvReader.getRows();
+  @Test
+  public void spacesAreConsideredPartOfAFieldAndShouldNotBeIgnored() {
+    CsvReader csvReader = new CsvReader("1997, Ford, E350\n");
+    List<List<String>> rows = csvReader.getRows();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(1));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(1));
 
-        List<String> rowValues = rows.get(0);
-        assertThat(rowValues, hasItems("1997", " Ford", " E350"));
-    }
+    List<String> rowValues = rows.get(0);
+    assertThat(rowValues, hasItems("1997", " Ford", " E350"));
+  }
 
-    @Test(expected = IllegalStateException.class)
-    public void quotesInAFieldAreNotAllowed() throws Exception {
-        CsvReader csvReader = new CsvReader("1997, \"Ford\" ,E350\n");
-        csvReader.getRows();
-    }
+  @Test(expected = IllegalStateException.class)
+  public void quotesInAFieldAreNotAllowed() {
+    CsvReader csvReader = new CsvReader("1997, \"Ford\" ,E350\n");
+    csvReader.getRows();
+  }
 
-    @Test
-    public void canUseHeaderRowToOrganizeValue() throws Exception {
-        CsvReader csvReader = new CsvReader("Year,Make,Model\n1997,Ford,E350\n2000,Mercury,Cougar\n");
-        List<Map<String, String>> rows = csvReader.getRowsBasedOnHeaders();
+  @Test
+  public void canUseHeaderRowToOrganizeValue() {
+    CsvReader csvReader = new CsvReader(
+        "Year,Make,Model\n1997,Ford,E350\n2000,Mercury,Cougar\n"
+    );
+    List<Map<String, String>> rows = csvReader.getRowsBasedOnHeaders();
 
-        assertThat(rows, is(notNullValue()));
-        assertThat(rows.size(), is(2));
+    assertThat(rows, is(notNullValue()));
+    assertThat(rows.size(), is(2));
 
-        Map<String, String> row0 = rows.get(0);
-        assertThat(row0.get("Year"), is("1997"));
-        assertThat(row0.get("Make"), is("Ford"));
-        assertThat(row0.get("Model"), is("E350"));
+    Map<String, String> row0 = rows.get(0);
+    assertThat(row0.get("Year"), is("1997"));
+    assertThat(row0.get("Make"), is("Ford"));
+    assertThat(row0.get("Model"), is("E350"));
 
-        Map<String, String> row1 = rows.get(1);
-        assertThat(row1.get("Year"), is("2000"));
-        assertThat(row1.get("Make"), is("Mercury"));
-        assertThat(row1.get("Model"), is("Cougar"));
-    }
+    Map<String, String> row1 = rows.get(1);
+    assertThat(row1.get("Year"), is("2000"));
+    assertThat(row1.get("Make"), is("Mercury"));
+    assertThat(row1.get("Model"), is("Cougar"));
+  }
+
+  @Test
+  public void shouldIgnoreLinesThatStartWithHash() {
+    List<List<String>> rows = new CsvReader(
+        "#1\n2\n#3\n4\n"
+    ).getRows();
+
+    assertThat(rows.size(), is(2));
+    assertThat(rows, hasItems(
+        hasItems("2"),
+        hasItems("4")
+    ));
+  }
+
+  @Test
+  public void shouldIgnoreLeadingCommentWhenChoosingHeader() {
+    List<Map<String, String>> rows = new CsvReader(
+        "#licence-comment\nnumber\n#1\n2\n#3\n4\n"
+    ).getRowsBasedOnHeaders();
+
+    assertThat(rows.size(), is(2));
+
+    List<String> numbers = rows.stream()
+        .map(stringStringMap -> stringStringMap.get("number"))
+        .collect(toList());
+    assertThat(numbers, hasItems("2", "4"));
+    assertThat(numbers, not(hasItem("1")));
+    assertThat(numbers, not(hasItem("3")));
+
+  }
 }
